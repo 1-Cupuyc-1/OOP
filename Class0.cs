76bef222fa3591ad912b84a8c3c749a63013fe5c
Артем <geraskinartem2005@gmail.com>
2025-06-15 02:27:02 +0300

    8 задание

--- Class0.cs
+++ Class0.cs
@@ -1,15 +1,9 @@
-﻿class Class0
+﻿class Class0 <T>
 {
-    public int number1 { get; set; } = 0;
-    public int number2 { get; set; } = 0;
-    public static Class0 operator+ (Class0 cls1, Class0 cls2)
+    public T field { get; set; }
+    public Class0(T field)
     {
-        int num1 = cls1.number1 + cls2.number1;
-        int num2 = cls1.number2 + cls2.number2;
-        Class0 new_class = new();
-        new_class.number1 = num1;
-        new_class.number2 = num2;
-        return new_class;
+        this.field = field;
     }
-
+    public void PrintTypeField() => Console.WriteLine($"{field.GetType().Name} field");
 }
--- Program.cs
+++ Program.cs
@@ -1,22 +1,16 @@
-﻿//Создать консольное приложение, в котором объявить класс, содержащий в себе два целочисленных автоматически реализуемых свойства.
-//Внутри класса переопределить оператор «+», который позволит складывать элементы данного класса между собой.  
+﻿//Создать консольное приложение, в котором объявить класс, использующий обобщенный тип данных.
+//Внутри класса объявить метод, который выводит на консоль название типа данных, который был использован в качестве обобщенного при создании элемента класса.  
 using System.Reflection;
 
 class Program
 {
     static void Main()
     {
-        Class0 cls1 = new(), cls2 = new();
-        Console.WriteLine($"cls1: \t number1 = {cls1.number1} \t number2 = {cls1.number2}");
-        Console.WriteLine($"cls2: \t number1 = {cls2.number1} \t number2 = {cls2.number2}\n");
-        cls1.number1 = 1;
-        cls1.number2 = 2;
-        cls2.number1 = 3;
-        cls2.number2 = 4;
-        Console.WriteLine($"cls1: \t number1 = {cls1.number1} \t number2 = {cls1.number2}");
-        Console.WriteLine($"cls2: \t number1 = {cls2.number1} \t number2 = {cls2.number2}\n");
-        Class0 cls3 = cls1 + cls2;
-        Console.WriteLine($"cls3: \t number1 = {cls3.number1} \t number2 = {cls3.number2}");
-
+        Class0<int> cls1 = new(1);
+        Class0<string> cls2 = new("1");
+        Class0<bool> cls3 = new(true);
+        cls1.PrintTypeField();
+        cls2.PrintTypeField();
+        cls3.PrintTypeField();
     }
 }
