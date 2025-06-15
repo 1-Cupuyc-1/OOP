02909a47a0a9a2763c0e55de07efbd207231406a
Артем <geraskinartem2005@gmail.com>
2025-06-15 17:08:15 +0300

    10 задание

--- Program.cs
+++ Program.cs
@@ -1,15 +1,35 @@
-﻿//Создать консольное приложение, в котором объявить класс, использующий обобщенный тип данных.
-//Дополнительно создать класс, внутри которого объявить конструктор, принимающий 2 параметра (целочисленный и строковый) и записывающий значения в автоматически реализуемые свойства.
-//Наложить ограничения на обобщенный тип данных, что он может принимать только описанный класс и классы наследники.  
+﻿//Создать консольное приложение, в котором объявить два делегата для хранения ссылок на методы, обладающие следующими сигнатурами: 
+//void(int, string, bool)
+//int(int[], double)
+//Реализовать методы, подходящие, описанным выше сигнатурам. Сохранить ссылки на данные в описанных делегатах. Вызвать методы используя созданные делегаты. 
 class Program
 {
+    delegate void Del1(int num, string str, bool boolean);
+    delegate int Del2(int[] a, double b);
     static void Main()
     {
-        Class1 cls = new(13, "Hello, World!");
-        Class2 CLS = new(0, "15.06.2025", false);
-        Class0<Class1> cls1 = new(cls);
-        Class0<Class2> cls2 = new(CLS);
-        cls1.PrintTypeField();
-        cls2.PrintTypeField();
+        void CheckRepresentation(int num, string str, bool boolean)
+        {
+            if (boolean == false) Console.WriteLine(str);
+            else Console.WriteLine("Вывод число можно представить в некомплексной форме: 13");
+        }
+        int AmountNum(int[] a, double b)
+        {
+            int quant = 0;
+            foreach(var num in a) if (num > b) quant++;
+            return quant;
+        }
+
+        int a = 13;
+        string b = "Число нельзя представить в некомплексной форме";
+        bool c = true;
+        int[] d = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        double e = 5.1;
+
+        Del1 del1 = CheckRepresentation;
+        Del2 del2 = new(AmountNum);
+
+        del1(a, b, c);
+        Console.WriteLine($"В массиве {del2?.Invoke(d, e)} числа, которые > {e}");
     }
 }
